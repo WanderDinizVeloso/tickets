2ed4f3aca409f10bbc442bbf19050adad103e2a4
Wander Veloso <wanveloso@yahoo.com.br>
2024-08-29 07:46:10 -0300

    refactor: refactor is-positive-decimal validator

--- src/class-validator-custom/is-positive-decimal.validator.ts
+++ src/class-validator-custom/is-positive-decimal.validator.ts
@@ -1,7 +1,22 @@
 import { registerDecorator, ValidationOptions, isDecimal } from 'class-validator';
-import * as ValidatorJS from 'validator';
 
 const FIRST_POSITION = 0;
+
+const ONE_DIGIT_DECIMAL = 1;
+
+interface IsDecimalOptions {
+  /**
+   * @default true
+   */
+  force_decimal?: boolean | undefined;
+  /**
+   * `decimal_digits` is given as a range like `'1,3'`,
+   * a specific value like `'3'` or min like `'1,'`
+   *
+   * @default '1'
+   */
+  decimal_digits?: string | undefined;
+}
 
 /**
  * Checks if the string is a valid positive decimal.
@@ -9,10 +24,10 @@ const FIRST_POSITION = 0;
  */
 export const IsPositiveDecimal =
   (
-    options?: ValidatorJS.IsDecimalOptions,
+    options?: IsDecimalOptions,
     validationOptions: ValidationOptions = {
       message: ({ property }) =>
-        `The '${property}' attribute is not a valid positive decimal number. It must be a string, positive and contain a two-digit decimal separated by a period. ex.: '1.65'.`,
+        `The '${property}' attribute is not a valid positive decimal number. It must be a string, positive and contain a ${Number(options?.decimal_digits) || ONE_DIGIT_DECIMAL} digit decimal separated by a period. ex.: '1.${Array.from(Array(Number(options?.decimal_digits) || ONE_DIGIT_DECIMAL).keys()).join('')}'.`,
     },
   ) =>
   (object: unknown, propertyName: string): void => {
@@ -20,7 +35,12 @@ export const IsPositiveDecimal =
       name: 'IsPositiveDecimal',
       target: object.constructor,
       propertyName: propertyName,
-      constraints: [options],
+      constraints: [
+        {
+          decimal_digits: options?.decimal_digits || '2',
+          force_decimal: options?.force_decimal || true,
+        },
+      ],
       options: validationOptions,
       validator: {
         validate: (value, args): boolean =>
