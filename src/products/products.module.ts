8b963f676caebc842df29d84581bffb4576168f2
Wander Veloso <wanveloso@yahoo.com.br>
2024-09-13 19:29:21 -0300

    feat: add monetary data service in products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { MonetaryDataModule } from '../monetary-data/monetary-data.module';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product, ProductSchema } from './schema/product.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MonetaryDataModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
-  exports: [],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
