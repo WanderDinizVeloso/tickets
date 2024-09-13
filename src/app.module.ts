3c21e1e7ffe6f9bb901bb458946e7f431bbd39af
Wander Veloso <wanveloso@yahoo.com.br>
2024-09-13 19:34:50 -0300

    feat: add order module and monetary data module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { MonetaryDataModule } from './monetary-data/monetary-data.module';
+import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://mongodb:27017/tickets'),
+    MonetaryDataModule,
+    OrdersModule,
     ProductsModule,
   ],
   controllers: [],
