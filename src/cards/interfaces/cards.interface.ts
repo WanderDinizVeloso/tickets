fb7debe8e8bb925a70b87a978955aab209e71184
Wander Veloso <wanveloso@yahoo.com.br>
2024-09-16 15:43:10 -0300

    feat: add cards interface

---
+++ src/cards/interfaces/cards.interface.ts
@@ -0,0 +1,19 @@
+export interface ICardPayload {
+  productId: string;
+  orderId: string;
+  name: string;
+  price: string;
+  quantity: string;
+}
+
+export interface ICardsCreateResponse {
+  ids: string[];
+  message: string;
+  statusCode: number;
+}
+
+export interface ICardDeleteResponse {
+  id: string;
+  message: string;
+  statusCode: number;
+}
