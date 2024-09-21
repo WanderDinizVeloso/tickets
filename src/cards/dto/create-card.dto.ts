5e62c37e334c2dcbff5aa6dd04ea7a38f60fe2d5
Wander Veloso <wanveloso@yahoo.com.br>
2024-09-20 23:23:08 -0300

    docs: add CreateCardsDTOSwagger in cards create order DTO

--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -2,9 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsNotEmpty } from 'class-validator';
 
 import { INVALID_ORDER_ID } from '../utils/string-literals.util';
+import { CreateCardsDTOSwagger } from '../swagger/cardsDTO.swagger';
 
 export class CreateCardDto {
-  @ApiProperty()
+  @ApiProperty(CreateCardsDTOSwagger.orderId.apiProperty)
   @IsNotEmpty()
   @IsMongoId({ message: INVALID_ORDER_ID })
   readonly orderId: string;
