9c0c9b59325dbf8e2ef0be451efe64d91b8bb467
Wander Veloso <wanveloso@yahoo.com.br>
2024-09-24 15:21:12 -0300

    feat: add tenants utils magic numbers and string liberals

---
+++ src/tenants/utils/tenants-string-literals.util.ts
@@ -0,0 +1,7 @@
+export const TENANT_CREATED_SUCCESSFULLY_RESPONSE = 'tenant created successfully.';
+
+export const TENANT_DELETED_SUCCESSFULLY_RESPONSE = 'tenant deleted successfully.';
+
+export const TENANT_EDITED_SUCCESSFULLY_RESPONSE = 'tenant edited successfully.';
+
+export const TENANT_NOT_EXIST_RESPONSE = 'tenant does not exist.';
