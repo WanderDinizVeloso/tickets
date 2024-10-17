af0bd7929b0fbc0d51348e7a8f1ea218e62bb07e
Wander Veloso <wanveloso@yahoo.com.br>
2024-10-17 18:36:18 -0300

    feat: add auth config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_GUARD } from '@nestjs/core';
 
+import { AuthGuard } from './guards/auth.guard';
+import { AuthModule } from './auth/auth.module';
 import { CardsModule } from './cards/cards.module';
+import { EncryptModule } from './encrypt/encrypt.module';
 import { MonetaryDataModule } from './monetary-data/monetary-data.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
@@ -9,15 +14,27 @@ import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://mongodb:27017/tickets'),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: Number(process.env.JWT_EXPIRES_AFTER_SECONDS) },
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
     CardsModule,
+    EncryptModule,
     MonetaryDataModule,
     OrdersModule,
     ProductsModule,
     TenantsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [],
 })
 export class AppModule {}
