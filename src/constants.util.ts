92210df070ad903663f3633707bfdd2ac7b645e1
Wander Veloso <wanveloso@yahoo.com.br>
2024-10-17 17:12:25 -0300

    refactor: add constants.util

---
+++ src/constants.util.ts
@@ -0,0 +1,62 @@
+export const BEARER = 'Bearer';
+
+export const CARDS_CREATED_SUCCESSFULLY_RESPONSE = 'cards were created successfully.';
+
+export const CARD_DELETED_SUCCESSFULLY_RESPONSE = 'card deleted successfully.';
+
+export const CARD_NOT_EXIST_RESPONSE = 'card does not exist.';
+
+export const CARDS_REGISTERED_RESPONSE =
+  'cards have already been created for the specified orderId.';
+
+export const INVALID_BEARER_TOKEN = 'invalid Bearer Token';
+
+export const INVALID_ID_RESPONSE = 'id attribute is invalid.';
+
+export const INVALID_ORDER_ID = 'orderId attribute is invalid.';
+
+export const INVALID_PASSWORD_RESPONSE =
+  'password must contain at least 8 (eight) characters, 01 (one) capital letter, 01 (one) lowercase letter, 01 (one) number and 01 (one) of the following special characters: #?!@$%^&*-';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+
+export const ONE = 1;
+
+export const ORDER_CREATED_SUCCESSFULLY_RESPONSE = 'order created successfully.';
+
+export const ORDER_DELETED_SUCCESSFULLY_RESPONSE = 'order deleted successfully.';
+
+export const ORDER_NOT_EXIST_RESPONSE = 'order does not exist.';
+
+export const PRODUCT_CREATED_SUCCESSFULLY_RESPONSE = 'product created successfully.';
+
+export const PRODUCT_DELETED_SUCCESSFULLY_RESPONSE = 'product deleted successfully.';
+
+export const PRODUCT_EDITED_SUCCESSFULLY_RESPONSE = 'product edited successfully.';
+
+export const PRODUCT_NOT_EXIST_RESPONSE = 'product does not exist.';
+
+export const PRODUCTS_NOT_REGISTERED_RESPONSE =
+  'there are products not registered in the order list.';
+
+export const REFRESH_TOKEN_INVALID_RESPONSE = 'refresh token is invalid';
+
+export const REPEATED_PRODUCT_IDS_RESPONSE = 'there are repeated productIds in the products list.';
+
+export const TENANT_CREATED_SUCCESSFULLY_RESPONSE = 'tenant created successfully.';
+
+export const TENANT_DELETED_SUCCESSFULLY_RESPONSE = 'tenant deleted successfully.';
+
+export const TENANT_EDITED_SUCCESSFULLY_RESPONSE = 'tenant edited successfully.';
+
+export const TENANT_NOT_EXIST_RESPONSE = 'tenant does not exist.';
+
+export const THREE_DIGITS = 3;
+
+export const TWO_DIGITS = 2;
+
+export const USER_CREATED_SUCCESSFULLY_RESPONSE = 'user created successfully.';
+
+export const WRONG_CREDENTIALS_RESPONSE = 'wrong credentials';
+
+export const ZERO = 0;
