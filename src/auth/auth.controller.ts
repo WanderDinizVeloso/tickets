6ed216657d0b50724428d0ad730dee2db524b793
Wander Veloso <wanveloso@yahoo.com.br>
2024-10-17 19:55:04 -0300

    docs: add auth controller swagger in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Post, Body, HttpStatus, HttpCode, Req } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+import { USER_CREATED_SUCCESSFULLY_RESPONSE } from '../constants.util';
 import { AuthService } from './auth.service';
+import { Public } from '../decorators-custom/public.decorator';
 import { LoginDto } from './dto/login.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { IAuthResponse, ITicketsRequest, IUserTokensResponse } from './interfaces/auth.interface';
-import { ApiTags } from '@nestjs/swagger';
-import { Public } from '../decorators-custom/public.decorator';
-import { USER_CREATED_SUCCESSFULLY_RESPONSE } from '../constants.util';
+import { AuthControllerSwagger } from './swagger/auth-controller.swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -17,6 +25,9 @@ export class AuthController {
   @Public()
   @Post('sign-up')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation(AuthControllerSwagger.postSignUp.apiOperation)
+  @ApiCreatedResponse(AuthControllerSwagger.postSignUp.apiCreatedResponse)
+  @ApiBadRequestResponse(AuthControllerSwagger.postSignUp.apiBadRequestResponse)
   async signUp(@Body() signUpDto: SignUpDto): Promise<IAuthResponse> {
     const id = await this.authService.signUp(signUpDto);
 
@@ -30,12 +41,20 @@ export class AuthController {
   @Public()
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation(AuthControllerSwagger.postLogin.apiOperation)
+  @ApiOkResponse(AuthControllerSwagger.postLogin.apiOkResponse)
+  @ApiBadRequestResponse(AuthControllerSwagger.postLogin.apiBadRequestResponse)
+  @ApiUnauthorizedResponse(AuthControllerSwagger.postLogin.apiUnauthorizedResponse)
   async login(@Body() loginDto: LoginDto): Promise<IUserTokensResponse> {
     return this.authService.login(loginDto);
   }
 
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation(AuthControllerSwagger.postRefreshToken.apiOperation)
+  @ApiOkResponse(AuthControllerSwagger.postRefreshToken.apiOKResponse)
+  @ApiBadRequestResponse(AuthControllerSwagger.postRefreshToken.apiBadRequestResponse)
+  @ApiUnauthorizedResponse(AuthControllerSwagger.postRefreshToken.apiUnauthorizedResponse)
   async refreshTokens(
     @Body() refreshTokenDto: RefreshTokenDto,
     @Req() req: ITicketsRequest,
