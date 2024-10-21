99e1d2b2c8c138d144a249445c3d8768a3f4fb07
Wander Veloso <wanveloso@yahoo.com.br>
2024-10-21 14:28:54 -0300

    feat: add resetToken collection config in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { EncryptModule } from '../encrypt/encrypt.module';
-import { User, UserSchema } from './schema/user.schema';
+import { MailModule } from '../mail/mail.module';
 import { RefreshToken, RefreshTokenSchema } from './schema/refresh-token.schema';
+import { ResetToken, ResetTokenSchema } from './schema/reset-token.schema';
+import { User, UserSchema } from './schema/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: RefreshToken.name, schema: RefreshTokenSchema },
+      { name: ResetToken.name, schema: ResetTokenSchema },
       { name: User.name, schema: UserSchema },
     ]),
     EncryptModule,
+    MailModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
